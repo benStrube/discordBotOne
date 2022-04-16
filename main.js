e911298b29d7beb9eb612e22cc794e379394f62e
Bstrube <benjaminj.strube@gmail.com>
2022-04-16 00:54:52 -0400

    prettier test 1

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
 const client = new Discord.Client();
 
-client.once('ready', () => {
-    console.log('DiscordBotOne is Online!');
-})
+client.once("ready", () => {
+  console.log("DiscordBotOne is Online!");
+});
 
-client.login('OTY0NzQ2NjMwOTk5OTY5ODgy.YlpIUA.IjIJGzHBuK4z3TThlZedUXea-Ug');
+client.login("OTY0NzQ2NjMwOTk5OTY5ODgy.YlpIUA.IjIJGzHBuK4z3TThlZedUXea-Ug");
